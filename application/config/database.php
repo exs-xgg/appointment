8c95700f1014a6f103698a34fbcc3752cf12fb70
Romeo David <zapperhatz26@gmail.com>
2019-10-21 15:01:55 +0800

    edit based on environment

--- application/config/database.php
+++ application/config/database.php
@@ -26,3 +26,24 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+$db2['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'drasicod_root',
+	'password' => 'rootmynigga',
+	'database' => 'drasicod_appointment',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
